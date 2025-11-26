eeb15c084572f2b9d4a0782c685026038d1b66a6
appinventiv <junsperspective@gmail.com>
2025-11-26 10:24:47 +0530

    feat: efficient-string-builing-js

---
+++ javascript/efficientStringBuilding.js
@@ -0,0 +1,18 @@
+// arr is a list of characters
+let fn = arr => {
+    let ans = [];
+    for (const c of arr) {
+        ans.push(c);
+    }
+
+    return ans.join("")
+}
+
+ fn = arr => {
+    let ans = "";
+    for (const c of arr) {
+        ans += c;
+    }
+
+    return ans;
+}
