e8334842b3241f419bb3a7d0b645407c34405047
appinventiv <junsperspective@gmail.com>
2025-11-27 10:57:11 +0530

    fix: backtracking-js

--- javascript/backtracking.js
+++ javascript/backtracking.js
@@ -1,19 +1,19 @@
 //Backtracking
-let backtrack = (curr, OTHER_ARGUMENTS...) => {
-    if (BASE_CASE) {
-        // modify the answer
-        return;
-    }
+// let backtrack = (curr, OTHER_ARGUMENTS...) => {
+//     if (BASE_CASE) {
+//         // modify the answer
+//         return;
+//     }
 
-    let ans = 0;
-    for (ITERATE_OVER_INPUT) {
-        // modify the current state
-        ans += backtrack(curr, OTHER_ARGUMENTS...);
-        // undo the modification of the current state
-    }
+//     let ans = 0;
+//     for (ITERATE_OVER_INPUT) {
+//         // modify the current state
+//         ans += backtrack(curr, OTHER_ARGUMENTS...);
+//         // undo the modification of the current state
+//     }
 
-    return ans;
-}
+//     return ans;
+// }
 // Example usage:
 // Define BASE_CASE and ITERATE_OVER_INPUT according to the problem requirements.
 // Call backtrack with initial parameters.
